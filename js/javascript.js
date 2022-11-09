b5022bdcb7f44dd196ac7fecf2bc60294c31bdef
will27nasc <willy27nascimento@gmail.com>
2022-11-09 17:46:11 -0300

    Construindo a section soft-skills

---
+++ js/javascript.js
@@ -0,0 +1,32 @@
+function mostrar1() {
+    if(comunicacao.style.display == 'block') {
+        comunicacao.style.display = 'none';
+    } else {
+        comunicacao.style.display = 'block';
+    }
+}
+
+function mostrar2() {
+    if(flexivel.style.display == 'block') {
+        flexivel.style.display = 'none';
+    } else {
+        flexivel.style.display = 'block';
+    }
+}
+
+function mostrar3() {
+    if(equipe.style.display == 'block') {
+        equipe.style.display = 'none';
+    } else {
+        equipe.style.display = 'block';
+    }
+}
+
+function mostrar4() {
+    if(criatividade.style.display == 'block') {
+        criatividade.style.display = 'none';
+    } else {
+        criatividade.style.display = 'block';
+    }
+}
+
